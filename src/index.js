b51d0743d93d28d6c2d5a59edf87e650857c6d04
Andrea Parodi <andrea@parro.it>
2015-11-10 21:53:30 +0100

    try / catch for each module required

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const path = require('path');
 
 const requireProps = baseDir => (target, modules) => {
   modules.forEach(modulePath => {
-    let m = modulePath;
-    if (m[0] === '.' || m[0] === path.sep) {
-      m = path.resolve(baseDir, m);
+    try {
+      let m = modulePath;
+      if (m[0] === '.' || m[0] === path.sep) {
+        m = path.resolve(baseDir, m);
+      }
+      const name = path.basename(m, path.extname(m));
+      target[name] = require(m)(target);
+    } catch (err) {
+      process.stdout.write(`While loading ${name}:\n${err.stack}\n\n`);
     }
-    const name = path.basename(m, path.extname(m));
-    target[name] = require(m)(target);
   });
 };
 
